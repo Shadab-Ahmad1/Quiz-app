4afc095eede0b47b3eb473b45b34690f145bcb78
Shadab-Baig <ashadab124@gmail.com>
2023-03-09 20:25:09 +0500

    quizApp: setting timer on it

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -10,6 +10,13 @@ import { QuestionService } from '../service/question.service';
 export class QuestionComponent implements OnInit {
 
   public name:string = "";
+  public questionList:any = [];
+  public currentQuestion:number = 0;
+  public points:number = 0;
+  public counter:number = 60;
+  public correctAnswer:number = 0;
+  public inCorrectAnswer:number = 0;
+  public interval$:any;
   constructor(private service : QuestionService) { }
 
   ngOnInit(): void {
@@ -20,8 +27,36 @@ export class QuestionComponent implements OnInit {
   getAllQuestions(){
     this.service.getQuestionJson().subscribe((result)=>{
       console.warn(result);
-      console.warn(result);
+      this.questionList = result.questions;
     })
   }
-
+  previousQuestion(){
+      this.currentQuestion--;
+    
+  }
+  nextQuestion(){
+    this.currentQuestion++;
+  }
+  answer(questionNo:number,option:any){
+    if(option.correct){
+      this.points+=10;
+      // this.points = this.points + 10;
+      this.correctAnswer++;
+      this.currentQuestion++
+    }
+    else{
+      this.points-= 10;
+      this.currentQuestion++;
+      this.inCorrectAnswer++;
+    }
+  }
+  startCounter(){
+    this.interval$ = interval
+  }
+  stopCounter(){
+    
+  }
+  resetCounter(){
+    
+  }
 }
